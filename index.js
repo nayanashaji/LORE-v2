d6ca70e7bc28c61136500f4a4bcc371bb6678b2c
nayanashaji <nayanashajim@gmail.com>
2026-02-14 22:18:39 +0530

    Basic style and navigation

---
+++ index.js
@@ -0,0 +1,6 @@
+let browseBtn=document.querySelector(".browse-btn");
+let signBtn=document.querySelector(".sign-btn");
+
+browseBtn.addEventListener("click",()=>{
+    window.location.href="home.html";
+})
