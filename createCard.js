d76981b2db1480ecc63f84916f5156db317a943e
nayanashaji <nayanashajim@gmail.com>
2026-02-16 00:26:29 +0530

    Styling cards

--- createCard.js
+++ createCard.js
@@ -1,10 +1,20 @@
-export let createCard=(cardData,cardContainer)=>{
+export let createCard=(cardData,cardsContainer)=>{
     for (let cardDetail of cardData){
+        const cardContainer=document.createElement("div");
+        cardContainer.classList.add("card-container");
+        
         const cardImgContainer=document.createElement("div");
         const cardImg=document.createElement("img");
+        cardImg.classList.add("card-img");
         cardImg.setAttribute("src",cardDetail.image);
-        cardImg.setAttribute("alt","img not found");
+        cardImg.setAttribute("alt",cardDetail.title);
         cardImgContainer.appendChild(cardImg);
         cardContainer.appendChild(cardImgContainer);
+
+        const title=document.createElement("h1");
+        title.innerText=cardDetail.title;
+        cardContainer.appendChild(title);
+
+        cardsContainer.appendChild(cardContainer);
     }
 }
